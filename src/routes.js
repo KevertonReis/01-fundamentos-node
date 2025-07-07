259add08eb3b0dd4ff68b77ff1bebf72c6fd964f
Keverton Reis <kevertondrv@gmail.com>
2025-07-07 17:02:28 -0300

    regex

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,24 @@
 import crypto from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
+
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    url: '/users',
-    handler: (req, res, database) => {
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
       const users = database.select('users')
       return res.end(JSON.stringify(users))
     }
   },
+
   {
     method: 'POST',
-    url: '/users',
-    handler: (req, res, database) => {
+    path: buildRoutePath('/users'),
+    handler: (req, res) => {
       const { name, email } = req.body
 
       const user = {
@@ -29,6 +32,14 @@ export const routes = [
       database.insert('users', user)
 
       return res.writeHead(201).end()
+    }
+  },
+
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      return res.end()
     }
   }
 ]
--- src/server.js
+++ src/server.js
@@ -5,6 +5,13 @@ import { routes } from "./routes.js";
 
 // UUID é um identificador único universal, usado para identificar de forma única um objeto ou entidade em sistemas distribuídos.
 
+//3 tipo de parametros de rota:
+//em URL
+// - Query Params: ?search=algumacoisa&limit=10 (usado para filtros, paginação, etc.)
+// - Route Params: /users/1 (usado para identificar um recurso específico) 
+//fora da URL 
+// - Request Body: envio de informaçoes de um formulario { "name": "John", "email": "}
+
 const database = new Database();
 
 const server = http.createServer(async (req, res) => {
@@ -13,10 +20,12 @@ const server = http.createServer(async (req, res) => {
   await json(req, res);
 
   const route = routes.find((route) => {
-    return route.method === method && route.url === url;
+    return route.method === method && route.path.test(url);
   });
 
   if (route) {
+    const routeParams = req.url.match(route.path); //extrai os parametros da rota
+
     return route.handler(req, res, database);
   }
 
